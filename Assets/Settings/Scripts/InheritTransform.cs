b8736c527a11b4e564a1bb5863128553233f7ff9
wanismoutri <wanismoutri123@gmail.com>
2026-06-24 12:38:15 -0700

    Added Unity Project

---
+++ Assets/Settings/Scripts/InheritTransform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritTransform : MonoBehaviour
+{
+    [SerializeField] private bool isPositionInherited;
+    [SerializeField] private bool isRotationInheritedX;
+    [SerializeField] private bool isRotationInheritedY;
+
+    [SerializeField] private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = isPositionInherited ? target.transform.localPosition : transform.localPosition;
+
+        //transform.localRotation = isRotationInherited ? target.transform.localRotation : transform.localRotation;
+
+        /*
+        transform.localRotation = Quaternion.Euler(isRotationInheritedX ? target.transform.localRotation.x : transform.localRotation.x,
+                                                   isRotationInheritedY ? target.transform.localRotation.y : transform.localRotation.y,
+                                                   0);
+        */
+
+        transform.eulerAngles = new Vector3(isRotationInheritedX ? target.transform.eulerAngles.x : transform.eulerAngles.x,
+                                            isRotationInheritedY ? target.transform.eulerAngles.y : transform.eulerAngles.y,
+                                            0);
+    }
+}
